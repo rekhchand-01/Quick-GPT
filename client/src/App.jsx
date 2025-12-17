92a43c6f27ba6e557f4c29c0874cc08f9c89f0b3
OCTANET_JUNE <rekhchand01@gmail.in>
2025-12-17 19:47:45 +0530

    created components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="flex h-screen w-screen">
+        <Sidebar />
+        <div className="flex-1">
+          <Routes>
+            <Route path='/' element={<ChatBox />} />
+            <Route path='/credits' element={<Credits />} />
+            <Route path='/community' element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+
+    </>
   )
 }
 
